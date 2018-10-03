5fdf5df13f7ac3af01ec0a8e5b4dabc46709e556
Johannes E. M. Mosig <jem-mosig@protonmail.com>
2018-10-03 18:45:01 +0200

    Time the different pools

--- try_pathos.py
+++ try_pathos.py
@@ -1,5 +1,6 @@
 
 import pathos
+from profiling import CheckpointTimer
 
 
 def sum_squared(a, b):
@@ -12,10 +13,26 @@ if __name__ == '__main__':
     mp = pathos.pools.ProcessPool()
     tp = pathos.pools.ThreadPool()
 
-    x = range(5)
-    y = range(0, 10, 2)
+    x = range(500)
+    y = range(0, 1000, 2)
 
-    for pool in [sp, pp, mp, tp]:
-        print(list(pool.map(sum_squared, x, y)))  # List is required for the SerialPool, since it is a generator
-        pool.close()  # Close the pool to any new jobs
-        pool.join()   # Cleanup the closed worker processes
+    with CheckpointTimer("pathos") as timer:
+        list(sp.map(sum_squared, x, y))  # List is required for the SerialPool, since it is a generator
+        sp.close()  # Close the pool to any new jobs
+        sp.join()   # Cleanup the closed worker processes
+        timer.checkpoint("Serial")
+
+        pp.map(sum_squared, x, y)
+        pp.close()  # Close the pool to any new jobs
+        pp.join()   # Cleanup the closed worker processes
+        timer.checkpoint("Parallel")
+
+        mp.map(sum_squared, x, y)
+        mp.close()  # Close the pool to any new jobs
+        mp.join()   # Cleanup the closed worker processes
+        timer.checkpoint("Process")
+
+        tp.map(sum_squared, x, y)
+        tp.close()  # Close the pool to any new jobs
+        tp.join()   # Cleanup the closed worker processes
+        timer.checkpoint("Thread")
